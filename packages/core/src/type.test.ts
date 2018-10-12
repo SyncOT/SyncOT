94394f6385fdfca8c825e8f4e36d311f13ee11cd
Greg Kubisa <gkubisa@gmail.com>
2018-10-12 22:32:19 +0200

    Add more tests

--- packages/core/src/type.test.ts
+++ packages/core/src/type.test.ts
@@ -1,37 +1,152 @@
 import { ErrorCodes, SyncOtError } from './error'
+import { Result } from './result'
 import { createTypeManager, Operation, Snapshot, Type, TypeManager } from './type'
 
-const name = 'test-type'
-const apply = (snapshot: Snapshot, _operation: Operation): Snapshot => snapshot
-const type: Type = { apply, name }
+interface NumberSnapshot extends Snapshot { data: number }
+interface NumberOperation extends Operation { data: number }
 
+const expectFail = (errorCode: ErrorCodes) => (result: Result<any>) => {
+    expect(result.isFail()).toBe(true)
+    expect(result.getError()).toBeInstanceOf(SyncOtError)
+    expect((result.getError() as SyncOtError).code).toBe(errorCode)
+}
+const expectTypeNotFound = expectFail(ErrorCodes.TypeNotFound)
+const expectNotImplemented = expectFail(ErrorCodes.NotImplemented)
+
+let type0: Type
+let type1: Type
+let type2: Type
+// let snapshot0: NumberSnapshot
+let snapshot1: NumberSnapshot
+// let snapshot2: NumberSnapshot
+let operation0: NumberOperation
+// let operation1: NumberOperation
+let operation2: NumberOperation
 let typeManager: TypeManager
 
 beforeEach(() => {
+    type0 = {
+        apply: jest.fn(),
+        applyX: jest.fn(),
+        invert: jest.fn(),
+        name: 'type0'
+    }
+    type1 = {
+        apply: jest.fn(),
+        applyX: jest.fn(),
+        invert: jest.fn(),
+        name: 'type1'
+    }
+    type2 = {
+        apply: jest.fn(),
+        applyX: jest.fn(),
+        invert: jest.fn(),
+        name: 'type2'
+    }
+    // snapshot0 = {
+    //     data: 0,
+    //     type: type0.name
+    // }
+    snapshot1 = {
+        data: 1,
+        type: type1.name
+    }
+    // snapshot2 = {
+    //     data: 2,
+    //     type: type2.name
+    // }
+    operation0 = {
+        data: 0,
+        type: type0.name
+    }
+    // operation1 = {
+    //     data: 1,
+    //     type: type1.name
+    // }
+    operation2 = {
+        data: 2,
+        type: type2.name
+    }
     typeManager = createTypeManager()
+    typeManager.registerType(type1)
+    typeManager.registerType(type2)
 })
 
 describe('registerType', () => {
     test('register a type', () => {
-        typeManager.registerType(type)
-        expect(typeManager.getType(name).getValue()).toBe(type)
+        typeManager.registerType(type0)
     })
-
     test('fail to register the same type twice', () => {
-        typeManager.registerType(type)
-        expect(() => typeManager.registerType(type)).toThrowError()
+        typeManager.registerType(type0)
+        expect(() => typeManager.registerType(type0)).toThrowError(`Duplicate type: ${type0.name}`)
     })
 })
 
 describe('getType', () => {
     test('get a type by name', () => {
-        typeManager.registerType(type)
-        expect(typeManager.getType(name).getValue()).toBe(type)
+        typeManager.registerType(type0)
+        expect(typeManager.getType(type0.name).getValue()).toBe(type0)
+        expect(typeManager.getType(type1.name).getValue()).toBe(type1)
+        expect(typeManager.getType(type2.name).getValue()).toBe(type2)
     })
-
     test('fail to get a non-existant type', () => {
-        const error = typeManager.getType(name).getError() as SyncOtError
-        expect(error).toBeInstanceOf(SyncOtError)
-        expect(error.code).toBe(ErrorCodes.TypeNotFound)
+        expectTypeNotFound(typeManager.getType(type0.name))
+    })
+})
+
+describe('apply', () => {
+    test('fails, if type not found', () => {
+        expectTypeNotFound(typeManager.apply(snapshot1, operation0))
+    })
+    test('calls Type#apply', () => {
+        (type2.apply as any).mockReturnValue(7)
+        expect(typeManager.apply(snapshot1, operation2).getValue()).toBe(7)
+        expect(type1.apply).not.toBeCalled()
+        expect(type2.apply).toHaveBeenCalledTimes(1)
+        expect((type2.apply as any).mock.instances[0]).toBe(type2)
+        expect(type2.apply).toHaveBeenCalledWith(snapshot1, operation2)
+    })
+})
+
+describe('applyX', () => {
+    test('fails, if type not found', () => {
+        expectTypeNotFound(typeManager.applyX(snapshot1, operation0))
+    })
+    test('calls Type#applyX', () => {
+        const returnValue = [ 7, 9 ];
+        (type2.applyX as any).mockReturnValue(returnValue)
+        expect(typeManager.applyX(snapshot1, operation2).getValue()).toBe(returnValue)
+        expect(type1.apply).not.toBeCalled()
+        expect(type1.applyX).not.toBeCalled()
+        expect(type1.invert).not.toBeCalled()
+        expect(type2.apply).not.toBeCalled()
+        expect(type2.applyX).toHaveBeenCalledTimes(1)
+        expect((type2.applyX as any).mock.instances[0]).toBe(type2)
+        expect(type2.applyX).toHaveBeenCalledWith(snapshot1, operation2)
+        expect(type2.invert).not.toBeCalled()
+    })
+    test('calls Type#apply and Type#invert', () => {
+        (type2.apply as any).mockReturnValue(7);
+        type2.applyX = undefined;
+        (type2.invert as any).mockReturnValue(9)
+        expect(typeManager.applyX(snapshot1, operation2).getValue()).toEqual([ 7, 9 ])
+        expect(type1.apply).not.toBeCalled()
+        expect(type1.applyX).not.toBeCalled()
+        expect(type1.invert).not.toBeCalled()
+        expect(type2.apply).toHaveBeenCalledTimes(1)
+        expect((type2.apply as any).mock.instances[0]).toBe(type2)
+        expect(type2.apply).toHaveBeenCalledWith(snapshot1, operation2)
+        expect(type2.invert).toHaveBeenCalledTimes(1)
+        expect((type2.invert as any).mock.instances[0]).toBe(type2)
+        expect(type2.invert).toHaveBeenCalledWith(operation2)
+    })
+    test('fails, if neither applyX nor invert are implemented', () => {
+        type2.applyX = undefined
+        type2.invert = undefined
+        expectNotImplemented(typeManager.applyX(snapshot1, operation2))
+        expect(type1.apply).not.toBeCalled()
+        expect(type1.applyX).not.toBeCalled()
+        expect(type1.invert).not.toBeCalled()
+        expect(type2.apply).not.toBeCalled()
     })
 })
