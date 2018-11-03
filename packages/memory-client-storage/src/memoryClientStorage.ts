0d48f2b11deb7ae6df0057c0cfc681b212ed49d2
Greg Kubisa <gkubisa@gmail.com>
2018-11-03 01:13:38 +0000

    memory-client-storage: Create module

---
+++ packages/memory-client-storage/src/memoryClientStorage.ts
@@ -0,0 +1,39 @@
+import {
+    ClientStorage,
+    DocumentId,
+    DocumentOperation,
+    DocumentVersion,
+    SequenceNumber
+} from '@syncot/core'
+
+export class MemoryClientStorage implements ClientStorage {
+    public saveRemoteOperations(
+        _id: DocumentId,
+        _operations: [DocumentOperation]
+    ): Promise<undefined> {
+        return Promise.reject(new Error('Not implemented'))
+    }
+
+    public loadRemoteOperations(
+        _id: DocumentId,
+        _start?: DocumentVersion,
+        _end?: DocumentVersion
+    ): Promise<[DocumentOperation]> {
+        return Promise.reject(new Error('Not implemented'))
+    }
+
+    public saveLocalOperations(
+        _id: DocumentId,
+        _operations: [DocumentOperation]
+    ): Promise<undefined> {
+        return Promise.reject(new Error('Not implemented'))
+    }
+
+    public loadLocalOperations(
+        _id: DocumentId,
+        _start?: SequenceNumber,
+        _end?: SequenceNumber
+    ): Promise<[DocumentOperation]> {
+        return Promise.reject(new Error('Not implemented'))
+    }
+}
