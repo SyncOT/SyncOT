968ebe45cffa5057fa76e343fd4a47ed405f0130
Greg Kubisa <gkubisa@gmail.com>
2019-02-07 11:38:36 +0100

    Make test coverage work again

--- jest.config.js
+++ jest.config.js
@@ -19,14 +19,14 @@ module.exports = {
                 },
                 testEnvironment: 'node',
                 testMatch: [`<rootDir>/packages/${name}/src/**/*.test.ts`],
-                collectCoverage: true,
-                collectCoverageFrom: [
-                    'packages/*/src/**/*.{ts,tsx,js,jsx}',
-                    '!packages/*/src/**/*.test.{ts,tsx,js,jsx}',
-                    '!packages/*/src/**/*Tests.{ts,tsx,js,jsx}',
-                    '!**/node_modules/**',
-                ],
             }
         }),
+    ],
+    collectCoverage: true,
+    collectCoverageFrom: [
+        '<rootDir>/packages/*/src/**/*.{ts,tsx,js,jsx}',
+        '!<rootDir>/packages/*/src/**/*.test.{ts,tsx,js,jsx}',
+        '!<rootDir>/packages/*/src/**/*Tests.{ts,tsx,js,jsx}',
+        '!<rootDir>/node_modules/',
     ],
 }
