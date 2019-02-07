c6ad48d47f1e7cf75e3e60c7f9e4f5d385183366
Greg Kubisa <gkubisa@gmail.com>
2019-02-07 01:24:55 +0100

    Improve some project config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,22 @@
+const { compilerOptions } = require('./tsconfig.json')
+
 module.exports = {
     preset: 'ts-jest',
     setupTestFrameworkScriptFile: 'jest-extended',
     moduleNameMapper: {
         '^@syncot/([-\\w]+)$': '<rootDir>/packages/$1/src',
     },
+    globals: {
+        'ts-jest': {
+            // jest uses the same tsConfig for all packages,
+            // so we have to ensure that it finds all the required definitions.
+            tsConfig: {
+                ...compilerOptions,
+                lib: ['dom'].concat(compilerOptions.lib),
+            },
+        },
+    },
+    testEnvironment: 'node',
     testPathIgnorePatterns: ['/node_modules/', '<rootDir>/packages/[^/]+/lib/'],
     collectCoverage: true,
     collectCoverageFrom: [
