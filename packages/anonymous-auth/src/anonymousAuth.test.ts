27e1b03a97ce6c4c17db7b5775ce0c3d819fc13a
Greg Kubisa <gkubisa@gmail.com>
2019-03-15 16:22:15 +0100

    Implement @syncot/anonymous-auth

---
+++ packages/anonymous-auth/src/anonymousAuth.test.ts
@@ -0,0 +1,177 @@
+import { AuthManager } from '@syncot/auth'
+import { Connection, createConnection } from '@syncot/core'
+import { binaryEqual } from '@syncot/util'
+import { Duplex } from 'stream'
+import { createAuthManager } from '.'
+
+const createDuplex = () =>
+    new Duplex({
+        read: () => undefined,
+        write: () => undefined,
+    })
+
+const userId = new ArrayBuffer(0)
+let stream: Duplex
+let connection: Connection
+let authManager: AuthManager
+
+beforeEach(() => {
+    stream = createDuplex()
+    connection = createConnection()
+})
+
+describe('right after create', () => {
+    test('initial state', () => {
+        authManager = createAuthManager(connection)
+        expect(authManager.getUserId()).toBeUndefined()
+        expect(authManager.hasUserId()).toBeFalse()
+        expect(authManager.hasAuthenticatedUserId()).toBeFalse()
+    })
+    test('mayRead', async () => {
+        authManager = createAuthManager(connection)
+        await expect(authManager.mayRead('', '')).resolves.toBeFalse()
+    })
+    test('mayWrite', async () => {
+        authManager = createAuthManager(connection)
+        await expect(authManager.mayWrite('', '')).resolves.toBeFalse()
+    })
+    test('events when not connected', async () => {
+        const onUser = jest.fn()
+        const onAuth = jest.fn()
+        authManager = createAuthManager(connection)
+        authManager.on('user', onUser)
+        authManager.on('auth', onAuth)
+        await Promise.resolve()
+        expect(onUser).toHaveBeenCalledTimes(1)
+        expect(onAuth).not.toHaveBeenCalled()
+    })
+    test('events when connected', async () => {
+        const onUser = jest.fn()
+        const onAuth = jest.fn()
+        connection.connect(stream)
+        authManager = createAuthManager(connection)
+        authManager.on('user', onUser)
+        authManager.on('auth', onAuth)
+        await Promise.resolve()
+        expect(onUser).toHaveBeenCalledTimes(1)
+        expect(onAuth).toHaveBeenCalledTimes(1)
+        expect(onAuth).toHaveBeenCalledAfter(onUser)
+    })
+    test('destroy', async () => {
+        const onUser = jest.fn()
+        const onAuth = jest.fn()
+        const onDestroy = jest.fn()
+        authManager = createAuthManager(connection)
+        authManager.on('user', onUser)
+        authManager.on('auth', onAuth)
+        authManager.on('destroy', onDestroy)
+        authManager.destroy()
+        await Promise.resolve()
+        expect(onDestroy).toHaveBeenCalledTimes(1)
+        expect(onUser).not.toHaveBeenCalled()
+        expect(onAuth).not.toHaveBeenCalled()
+        expect(authManager.getUserId()).toBeUndefined()
+        expect(authManager.hasUserId()).toBeFalse()
+        expect(authManager.hasAuthenticatedUserId()).toBeFalse()
+        await expect(authManager.mayRead('', '')).resolves.toBeFalse()
+        await expect(authManager.mayWrite('', '')).resolves.toBeFalse()
+    })
+    test('destroy twice', async () => {
+        const onDestroy = jest.fn()
+        authManager = createAuthManager(connection)
+        authManager.on('destroy', onDestroy)
+        authManager.destroy()
+        authManager.destroy()
+        await Promise.resolve()
+        expect(onDestroy).toHaveBeenCalledTimes(1)
+    })
+    test('destroy on user event', async () => {
+        const onAuth = jest.fn()
+        const onDestroy = jest.fn()
+        authManager = createAuthManager(connection)
+        authManager.on('user', () => {
+            authManager.destroy()
+        })
+        authManager.on('auth', onAuth)
+        authManager.on('destroy', onDestroy)
+        connection.connect(stream)
+        await Promise.resolve()
+        expect(onAuth).not.toHaveBeenCalled()
+        expect(onDestroy).toHaveBeenCalledTimes(1)
+    })
+})
+
+describe('initially disconnected', () => {
+    beforeEach(() => {
+        authManager = createAuthManager(connection)
+    })
+
+    test('initial state', async () => {
+        expect(binaryEqual(authManager.getUserId()!, userId)).toBeTrue()
+        expect(authManager.hasUserId()).toBeTrue()
+        expect(authManager.hasAuthenticatedUserId()).toBeFalse()
+    })
+    test('mayRead', async () => {
+        await expect(authManager.mayRead('', '')).resolves.toBeTrue()
+    })
+    test('mayWrite', async () => {
+        await expect(authManager.mayWrite('', '')).resolves.toBeTrue()
+    })
+    test('connect', async () => {
+        const onAuth = jest.fn()
+        authManager.on('auth', onAuth)
+        connection.connect(stream)
+        expect(onAuth).toHaveBeenCalledTimes(1)
+        expect(authManager.hasAuthenticatedUserId()).toBeTrue()
+    })
+    test('destroy', async () => {
+        const onDestroy = jest.fn()
+        authManager.on('destroy', onDestroy)
+        authManager.destroy()
+        await Promise.resolve()
+        expect(onDestroy).toHaveBeenCalledTimes(1)
+        expect(authManager.getUserId()).toBeUndefined()
+        expect(authManager.hasUserId()).toBeFalse()
+        expect(authManager.hasAuthenticatedUserId()).toBeFalse()
+        await expect(authManager.mayRead('', '')).resolves.toBeFalse()
+        await expect(authManager.mayWrite('', '')).resolves.toBeFalse()
+    })
+})
+
+describe('initially connected', () => {
+    beforeEach(() => {
+        connection.connect(stream)
+        authManager = createAuthManager(connection)
+    })
+
+    test('initial state', async () => {
+        expect(binaryEqual(authManager.getUserId()!, userId)).toBeTrue()
+        expect(authManager.hasUserId()).toBeTrue()
+        expect(authManager.hasAuthenticatedUserId()).toBeTrue()
+    })
+    test('mayRead', async () => {
+        await expect(authManager.mayRead('', '')).resolves.toBeTrue()
+    })
+    test('mayWrite', async () => {
+        await expect(authManager.mayWrite('', '')).resolves.toBeTrue()
+    })
+    test('disconnect', () => {
+        const onAuthEnd = jest.fn()
+        authManager.on('authEnd', onAuthEnd)
+        connection.disconnect()
+        expect(onAuthEnd).toHaveBeenCalledTimes(1)
+        expect(authManager.hasAuthenticatedUserId()).toBeFalse()
+    })
+    test('destroy', async () => {
+        const onDestroy = jest.fn()
+        authManager.on('destroy', onDestroy)
+        authManager.destroy()
+        await Promise.resolve()
+        expect(onDestroy).toHaveBeenCalledTimes(1)
+        expect(authManager.getUserId()).toBeUndefined()
+        expect(authManager.hasUserId()).toBeFalse()
+        expect(authManager.hasAuthenticatedUserId()).toBeFalse()
+        await expect(authManager.mayRead('', '')).resolves.toBeFalse()
+        await expect(authManager.mayWrite('', '')).resolves.toBeFalse()
+    })
+})
---
+++ packages/anonymous-auth/src/anonymousAuth.ts
@@ -0,0 +1,96 @@
+import { AuthEvents, AuthManager, UserId } from '@syncot/auth'
+import { Connection } from '@syncot/core'
+import { EventEmitter } from 'events'
+import { NodeEventEmitter } from '../../util/lib'
+
+/**
+ * Creates a new AuthManager which allows anonymous read-write access to all documents.
+ * It's suitable for use on both the client and server sides.
+ */
+export function createAuthManager(connection: Connection): AuthManager {
+    return new AnonymousAuthManager(connection)
+}
+
+/**
+ * An auth module providing anonymous read-write access to all documents.
+ * It's suitable for use on both the client and server sides.
+ */
+class AnonymousAuthManager
+    extends (EventEmitter as new () => NodeEventEmitter<AuthEvents>)
+    implements AuthManager {
+    private userId: UserId | undefined = undefined
+    private authenticated: boolean = false
+    private destroyed: boolean = false
+
+    public constructor(private connection: Connection) {
+        super()
+        Promise.resolve().then(() => this.init())
+    }
+
+    public getUserId(): UserId | undefined {
+        return this.userId
+    }
+
+    public hasUserId(): boolean {
+        return !!this.userId
+    }
+
+    public hasAuthenticatedUserId(): boolean {
+        return this.authenticated
+    }
+
+    public async mayRead(): Promise<boolean> {
+        return this.hasUserId()
+    }
+
+    public async mayWrite(): Promise<boolean> {
+        return this.hasUserId()
+    }
+
+    public destroy(): void {
+        if (this.destroyed) {
+            return
+        }
+        this.destroyed = true
+        this.userId = undefined
+        this.authenticated = false
+        this.connection.off('connect', this.onConnect)
+        this.connection.off('disconnect', this.onDisconnect)
+        this.emit('destroy')
+    }
+
+    private init(): void {
+        if (this.destroyed) {
+            return
+        }
+
+        this.userId = new ArrayBuffer(0)
+        this.emit('user')
+
+        if (this.destroyed) {
+            return
+        }
+
+        this.connection.on('connect', this.onConnect)
+        this.connection.on('disconnect', this.onDisconnect)
+
+        this.authenticated = this.connection.isConnected()
+        if (this.authenticated) {
+            this.emit('auth')
+        }
+    }
+
+    private onConnect = () => {
+        if (!this.destroyed && !this.authenticated) {
+            this.authenticated = true
+            this.emit('auth')
+        }
+    }
+
+    private onDisconnect = () => {
+        if (!this.destroyed && this.authenticated) {
+            this.authenticated = false
+            this.emit('authEnd')
+        }
+    }
+}
