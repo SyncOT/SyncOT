53aa921d7d8dd81f83f7ecd290e78c38204ac41c
Greg Kubisa <gkubisa@gmail.com>
2019-05-21 15:20:01 +0200

    Implement a composite connection stream factory

---
+++ packages/connection/src/factory.test.ts
@@ -0,0 +1,56 @@
+import { Duplex } from 'readable-stream'
+import { createStream, StreamFactory } from '.'
+
+const read = () => undefined
+const write = (
+    _data: any,
+    _encoding: string,
+    callback: (error: Error | undefined) => void,
+) => callback(undefined)
+
+const streams: { [name: string]: Duplex } = {
+    stream1: new Duplex({ read, write }),
+    stream2: new Duplex({ read, write }),
+    stream3: new Duplex({ read, write }),
+}
+
+const errors: { [name: string]: Error } = {
+    error1: new Error('test error 1'),
+    error2: new Error('test error 2'),
+    error3: new Error('test error 3'),
+}
+
+const factories: { [name: string]: StreamFactory } = {
+    fail1: () => Promise.reject(errors.error1),
+    fail2: () => Promise.reject(errors.error2),
+    fail3: () => Promise.reject(errors.error3),
+    ok1: () => Promise.resolve(streams.stream1),
+    ok2: () => Promise.resolve(streams.stream2),
+    ok3: () => Promise.resolve(streams.stream3),
+}
+
+test.each<[string[], string, string[]]>([
+    [[], '', []],
+    [['ok1'], 'stream1', []],
+    [['ok1', 'ok2', 'ok3'], 'stream1', []],
+    [['ok1', 'ok2', 'ok3', 'fail1', 'fail2', 'fail3'], 'stream1', []],
+    [['fail1', 'fail2', 'fail3', 'ok1', 'ok2', 'ok3'], 'stream1', []],
+    [['fail1', 'fail2', 'fail3'], '', ['error1', 'error2', 'error3']],
+])('factories=%p', async (factoryNames, streamName, errorNames) => {
+    const factoryList = factoryNames.map(name => factories[name])
+    const stream = streams[streamName]
+    const errorList = errorNames.map(name => errors[name])
+    const promise = createStream(factoryList)()
+
+    if (stream) {
+        await expect(promise).resolves.toBe(stream)
+    } else {
+        await expect(promise).rejects.toEqual(
+            expect.objectContaining({
+                errors: errorList,
+                message: 'Failed to create a stream.',
+                name: 'SyncOtError Composite',
+            }),
+        )
+    }
+})
---
+++ packages/connection/src/factory.ts
@@ -0,0 +1,33 @@
+import { createCompositeError } from '@syncot/error'
+import { Duplex } from 'readable-stream'
+
+/**
+ * The type of functions for creating streams expected by the `ConnectionManager`.
+ */
+export type StreamFactory = () => Promise<Duplex>
+
+/**
+ * Tries the specified stream `factories` in order until one succeeds,
+ * and returns the created stream.
+ * @param factories A list of stream factories.
+ * @returns A stream created by one of the specified stream `factories`.
+ */
+export const createStream = (
+    factories: StreamFactory[],
+): StreamFactory => () => {
+    const errors: Error[] = []
+    let index = 0
+    async function tryNext(): Promise<Duplex> {
+        if (index < factories.length) {
+            try {
+                return await factories[index++]()
+            } catch (error) {
+                errors.push(error)
+                return tryNext()
+            }
+        } else {
+            throw createCompositeError('Failed to create a stream.', errors)
+        }
+    }
+    return tryNext()
+}
