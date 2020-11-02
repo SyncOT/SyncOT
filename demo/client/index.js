b0506c8b2847b70fff5d968fb1a8d3d934e1e908
Greg Kubisa <gkubisa@gmail.com>
2020-11-02 13:09:52 +0100

    chore: set up a basic ProseMirror demo

---
+++ demo/client/index.js
@@ -0,0 +1,33 @@
+import './index.css'
+import 'prosemirror-view/style/prosemirror.css'
+import { Buffer } from 'buffer'
+import { decode } from '@syncot/tson'
+import { baseKeymap } from 'prosemirror-commands'
+import { undo, redo, history } from 'prosemirror-history'
+import { keymap } from 'prosemirror-keymap'
+import { schema } from 'prosemirror-schema-basic'
+import { EditorState } from 'prosemirror-state'
+import { EditorView } from 'prosemirror-view'
+
+window.Buffer = Buffer
+
+// Expose a TSON decoder - handy for decoding WebSocket messages in dev-tools.
+window.tsonDecodeBase64 = base64 => decode(Buffer.from(base64, 'base64'))
+window.tsonDecodeHex = hex => decode(Buffer.from(hex, 'hex'))
+window.tsonDecodeUtf8 = utf8 => decode(Buffer.from(utf8, 'utf8'))
+
+const isWin = /Win/.test(navigator.platform)
+const historyKeyMap = {
+    'Mod-z': undo,
+    'Mod-Shift-z': redo,
+}
+if (isWin) {
+    historyKeyMap['Mod-y'] = redo
+}
+const state = EditorState.create({
+    schema,
+    plugins: [history(), keymap(historyKeyMap), keymap(baseKeymap)],
+})
+const view = new EditorView(document.body, { state })
+
+window.editor = view
---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import commonjs from '@rollup/plugin-commonjs'
+import html from '@rollup/plugin-html'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import liveReload from 'rollup-plugin-livereload'
+import postcss from 'rollup-plugin-postcss'
+import serve from 'rollup-plugin-serve'
+
+const isWatchMode = process.env.ROLLUP_WATCH === 'true'
+const shouldIncludeSourceMaps = process.env.SOURCEMAPS === 'true'
+const src = 'demo/client'
+const dst = 'demo/client-dist'
+
+export default {
+    input: `${src}/index.js`,
+    output: {
+        dir: dst,
+        sourcemap: shouldIncludeSourceMaps,
+        format: 'esm',
+    },
+    plugins: [
+        commonjs(),
+        nodeResolve({ preferBuiltins: false }),
+        postcss({ extract: true }),
+        html({ title: 'SyncOT demo' }),
+        isWatchMode && liveReload({ watch: dst }),
+        isWatchMode && serve(dst),
+    ],
+}
