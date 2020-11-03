0d59fa83d261e428eadfce66151a001b5a36bdce
Greg Kubisa <gkubisa@gmail.com>
2020-11-03 08:45:23 +0100

    chore: remove redundant packages

--- packages/client-storage/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './clientStorage'
-export * from './clientStorageTests'
--- packages/memory-client-storage/src/clientStorage.test.ts
+++
@@ -1,4 +0,0 @@
-import { clientStorageTests } from '@syncot/client-storage'
-import { createClientStorage } from '.'
-
-clientStorageTests(createClientStorage)
