09003255fe4b76b2259ac2eb761de1d0afa47563
Greg Kubisa <gkubisa@gmail.com>
2020-11-13 18:15:53 +0100

    revert: remove PresenceError
    BREAKING CHANGE: PresenceError moved to @syncot/presence

--- packages/error/src/error.test.ts
+++ packages/error/src/error.test.ts
@@ -5,7 +5,6 @@ import {
     createError,
     createInvalidEntityError,
     createInvalidStreamError,
-    createPresenceError,
     createSocketError,
     CustomError,
     fromJSON,
@@ -15,7 +14,6 @@ import {
     isCustomError,
     isInvalidEntityError,
     isInvalidStreamError,
-    isPresenceError,
     isSocketError,
     isSyncOTError,
     toJSON,
@@ -315,34 +313,6 @@ describe('InvalidEntityError', () => {
         expect(isInvalidEntityError(error)).toBeTrue()
         expect(isInvalidEntityError(new Error())).toBeFalse()
         expect(isInvalidEntityError({})).toBeFalse()
-    })
-})
-
-describe('PresenceError', () => {
-    test('createPresenceError', () => {
-        const error = createPresenceError('test')
-        expect(error).toBeInstanceOf(Error)
-        expect(error.name).toBe('SyncOTError Presence')
-        expect(error.message).toBe('test')
-        expect(error.cause).toBe(undefined)
-    })
-    test('createPresenceError with cause', () => {
-        const cause = new Error('Test cause!')
-        const error = createPresenceError('Test message.', cause)
-        expect(error).toBeInstanceOf(Error)
-        expect(error.name).toBe('SyncOTError Presence')
-        expect(error.message).toBe('Test message. => Error: Test cause!')
-        expect(error.toString()).toBe(
-            'SyncOTError Presence: Test message. => Error: Test cause!',
-        )
-        expect(error.cause).toBe(cause)
-    })
-    test('isPresenceError', () => {
-        const error = createPresenceError('test')
-        expect(isSyncOTError(error)).toBeTrue()
-        expect(isPresenceError(error)).toBeTrue()
-        expect(isPresenceError(new Error())).toBeFalse()
-        expect(isPresenceError({})).toBeFalse()
     })
 })
 
--- packages/error/src/error.ts
+++ packages/error/src/error.ts
@@ -173,24 +173,6 @@ export function isInvalidEntityError(error: any): error is InvalidEntityError {
     return isCustomError(error) && error.name === 'SyncOTError InvalidEntity'
 }
 
-export interface PresenceError extends Error {
-    cause?: Error
-    name: 'SyncOTError Presence'
-}
-export function createPresenceError(
-    message?: string,
-    cause?: Error,
-): PresenceError {
-    return createError({
-        cause,
-        message,
-        name: 'SyncOTError Presence',
-    }) as PresenceError
-}
-export function isPresenceError(error: any): error is PresenceError {
-    return isCustomError(error) && error.name === 'SyncOTError Presence'
-}
-
 export interface AuthError extends Error {
     cause?: Error
     name: 'SyncOTError Auth'
