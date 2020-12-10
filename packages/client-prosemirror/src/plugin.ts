5ea8d2b8a3979f011a940f4e295eb446dfb534c3
Greg Kubisa <gkubisa@gmail.com>
2020-12-10 13:10:34 +0100

    refactor: import workLoop from @syncot/util

--- packages/client-prosemirror/src/plugin.ts
+++ packages/client-prosemirror/src/plugin.ts
@@ -4,7 +4,13 @@ import {
     isAlreadyExistsError,
     Operation,
 } from '@syncot/content'
-import { assert, delay, noop, throwError } from '@syncot/util'
+import {
+    assert,
+    exponentialBackOffStrategy,
+    noop,
+    throwError,
+    workLoop,
+} from '@syncot/util'
 import OrderedMap from 'orderedmap'
 import { MarkSpec, NodeSpec } from 'prosemirror-model'
 import {
@@ -18,158 +24,6 @@ import {
 import { Step, Transform } from 'prosemirror-transform'
 import { EditorView } from 'prosemirror-view'
 import { Duplex } from 'readable-stream'
-
-/**
- * Returns the number of milliseconds to wait before the specified retry attempt.
- * @param retryAttempt The 0-based retry attempt number.
- * @returns The number of milliseconds to wait.
- */
-export type BackOffStrategy = (retryAttempt: number) => number
-
-/**
- * The options expected by the `exponentialBackOffStrategy` function.
- */
-export interface ExponentialBackOffStrategyOptions {
-    /**
-     * The minimum delay in milliseconds, defaults to 1000.
-     */
-    minDelay?: number
-    /**
-     * The maximum delay in milliseconds, defaults to 10000.
-     */
-    maxDelay?: number
-    /**
-     * The delay factor, defaults to 1.5.
-     */
-    delayFactor?: number
-}
-
-/**
- * Creates a function which implements the exponential back-off strategy with fixed configuration.
- * @param options Options for configuring the strategy.
- * @returns A function which implement the exponential back-off strategy.
- */
-export function exponentialBackOffStrategy(
-    options?: ExponentialBackOffStrategyOptions,
-): BackOffStrategy {
-    const { minDelay = 1000, maxDelay = 10000, delayFactor = 1.5 } =
-        options || {}
-    assert(
-        Number.isSafeInteger(minDelay) && minDelay >= 0,
-        '"minDelay" must be a safe integer >= 0.',
-    )
-    assert(
-        Number.isSafeInteger(maxDelay) && maxDelay >= minDelay,
-        '"maxDelay" must be a safe integer >= minDelay.',
-    )
-    assert(
-        Number.isFinite(delayFactor),
-        '"delayFactor" must be a finite number.',
-    )
-    return (attempt: number) =>
-        Math.max(
-            minDelay,
-            Math.min(
-                maxDelay,
-                Math.floor(minDelay * Math.pow(delayFactor, attempt)),
-            ),
-        )
-}
-
-/**
- * A WorkLoop performs work in an iterative fasion.
- */
-export interface WorkLoop {
-    /**
-     * Performs some work in an iteration of the WorkLoop.
-     * Defaults to do nothing.
-     * @param notify A function which notifies that there's more work to do.
-     * @returns If it returns a Promise,
-     *   the WorkLoop waits until it is fulfilled before starting the next iteration.
-     */
-    work?(notify: () => void): void | Promise<void>
-    /**
-     * Releases the resources claimed by the WorkLoop.
-     * Defaults to do nothing.
-     * @param notify A function which notifies that there's more work to do.
-     */
-    destroy?(notify: () => void): void
-    /**
-     * Informs whether all works has been completed or more should be expected in the future.
-     * Defaults to a function which always returns `false`.
-     * @returns `true`, if all work has been completed and the WorkLoop should be terminated, otherwise `false`.
-     */
-    isDone?(): boolean
-    /**
-     * Reports an error.
-     * Defaults to a function which throws the error.
-     * @param error The error to report.
-     */
-    onError?(error: Error): void
-    /**
-     * Returns the max number of milliseconds to wait before retrying a failed iteration of the WorkLoop.
-     * Defaults to no maximum delay.
-     */
-    retryDelay?: BackOffStrategy
-}
-
-/**
- * Manages a WorkLoop obtained by calling `create`.
- *
- * It works as follows:
- *
- * 1. Call `isDone`. If it returns `true`, call `destroy` and exit.
- * 2. Call `work` and wait until it's complete.
- * 3. If `work` succeeds,
- *   - reset `retryAttempt` back to 0,
- *   - wait until notified and resume at step 1.
- * 4. If `work` fails,
- *   - call `onError` to report the error,
- *   - get `maxDelay` by calling `retryDelay(retryAttemp)`,
- *   - increment `retryAttempt`,
- *   - wait for at most `maxDelay` or until notified and resume at step 1.
- *
- * @param create Returns an instance of WorkLoop to manage - usually a new instance.
- *   This function is always called synchronously by `workLoop`.
- *   The functions of the returned `WorkLoop` instance are always called asynchronously.
- *   It gets a `notify` function which can be called at any time
- *   to notify the loop that there is more work to do.
- *   The same `notify` function is passed to `WorkLoop#work` and `WorkLoop#destroy`.
- * @returns A Promise which resolves once the loop completes.
- */
-export async function workLoop<T extends WorkLoop>(
-    create: (notify: () => void) => T,
-): Promise<void> {
-    let retryAttempt = 0
-    let change: Promise<void>
-    let triggerChange = noop
-    const notify = () => triggerChange()
-    const instance = create(notify)
-    try {
-        await Promise.resolve()
-        while (!(instance.isDone && instance.isDone())) {
-            change = new Promise((resolve) => (triggerChange = resolve))
-            try {
-                if (instance.work) await instance.work(notify)
-                await change
-                retryAttempt = 0
-            } catch (error) {
-                queueMicrotask(() => {
-                    if (instance.onError) instance.onError(error)
-                    else throw error
-                })
-                if (instance.retryDelay)
-                    await Promise.race([
-                        change,
-                        delay(instance.retryDelay(retryAttempt++)),
-                    ])
-                else await change
-            }
-        }
-    } finally {
-        if (instance.destroy) instance.destroy(notify)
-    }
-}
 
 /**
  * The config expected by the `syncOT` plugin.
